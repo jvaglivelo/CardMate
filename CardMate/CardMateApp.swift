3d8ac40d997568b1ad651fb5da52770e1f2a8817
Jordan Vaglivelo <jordanvaglivelo@gmail.com>
2020-12-06 11:39:55 -0500

    Initial Commit

---
+++ CardMate/CardMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardMateApp.swift
+//  CardMate
+//
+//  Created by Jordan Vaglivelo on 12/6/20.
+//
+
+import SwiftUI
+
+@main
+struct CardMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
