c106dccefb7a2946b538411e06d65f16987e9e8b
Jordan Vaglivelo <jordanvaglivelo@gmail.com>
2020-12-07 15:27:28 -0500

    Create SettingsView

---
+++ CardMate/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  CardMate
+//
+//  Created by Jordan Vaglivelo on 12/7/20.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
