92fae004518e2927dd625108812c5346851a752c
Jordan Vaglivelo <jordanvaglivelo@gmail.com>
2020-12-08 12:40:07 -0500

    Create ResultsView

---
+++ CardMate/ResultsView.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsView.swift
+//  CardMate
+//
+//  Created by Jordan Vaglivelo on 12/8/20.
+//
+
+import SwiftUI
+
+struct ResultsView: View {
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultsView()
+    }
+}
